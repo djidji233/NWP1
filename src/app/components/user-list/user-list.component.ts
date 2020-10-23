185f003c6a928f9ec863dcd1dbe7037cb9a20c30
Djordje Zivanovic <djolezile@gmail.com>
2020-10-23 14:50:10 +0200

    Add User works. Add Group works! Improve user-details and groupservice

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,7 +1,10 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import { User } from 'src/app/models/user.model';
-import { UserService } from 'src/app/services/user/user.service';
+import {User} from 'src/app/models/user.model';
+import {UserService} from 'src/app/services/user/user.service';
 import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Group} from '../../models/group.model';
+import {GroupService} from '../../services/group/group.service';
 
 @Component({
   selector: 'app-user-list',
@@ -10,27 +13,76 @@ import {Router} from '@angular/router';
 })
 export class UserListComponent implements OnInit {
 
-  public users: User[]
+  public users: User[];
+  public user = new User(-1, '', '');
+  public group = new Group([], '');
+  public userAddForm: FormGroup;
+  public groupAddForm: FormGroup;
 
   // Pomocu parametra u konstruktoru injektujemo UserService instancu u UserListComponent
-  constructor(private userService: UserService,private router: Router) { }
+  constructor(private userService: UserService,
+              private groupService: GroupService,
+              private router: Router,
+              private formBuilder: FormBuilder) {
+    this.validateAddUserForm(this.user.firstName, this.user.lastName);
+    this.validateAddGroupForm(this.group.groupName);
+  }
 
   ngOnInit(): void {
-    this.fetch()
+    this.fetch();
   }
 
   fetch() {
     this.userService.fetchUsers().subscribe(users => {
-      console.log(users)
-      this.users = users
-    })
+      console.log(users);
+      this.users = users;
+    });
+  }
+
+  validateAddUserForm(first: string, last: string) {
+    this.userAddForm = this.formBuilder.group({
+      userFirstName: [first, Validators.required],
+      userLastName: [last, Validators.required]
+    });
+  }
+
+  validateAddGroupForm(name: string) {
+    this.groupAddForm = this.formBuilder.group({
+      groupName: [name, Validators.required]
+    });
   }
 
   deleteUser(id: number) {
     this.userService.removeUser(id).subscribe(user => {
-      console.log(user)
-      this.fetch()
-    })
+      console.log(user);
+      this.fetch();
+    });
+  }
+
+  public get userFirstName() {
+    return this.userAddForm.get('userFirstName');
+  }
+
+  public get userLastName() {
+    return this.userAddForm.get('userLastName');
+  }
+
+  public get groupName() {
+    return this.groupAddForm.get('groupName');
+  }
+
+  public submitAddUserForm(credentials) {
+    console.log('submit add user form');
+    this.userService.addUser(credentials).subscribe(data => {
+      this.fetch();
+      this.validateAddUserForm('', '');
+    });
+  }
+
+  public submitAddGroupForm(credentials) {
+    console.log(credentials.groupName);
+    this.groupService.addGroup(credentials.groupName);
+    this.validateAddGroupForm('');
   }
 
 }
--- src/app/services/group/group.service.ts
+++ src/app/services/group/group.service.ts
@@ -1,34 +1,51 @@
-import { Injectable } from '@angular/core';
-import { User } from 'src/app/models/user.model';
-import { Group } from 'src/app/models/group.model';
+import {Injectable} from '@angular/core';
+import {User} from 'src/app/models/user.model';
+import {Group} from 'src/app/models/group.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
 
-  private group: Group
+  private groups: Group[] = [];
+
+  //private group: Group
 
   constructor() {
-    this.group = new Group([], "404")
+    this.groups.push(new Group([], '1'));
+    this.groups.push(new Group([], '2'));
+    this.groups.push(new Group([], '3'));
+    //this.group = this.groups[0];
   }
 
-  public getGroupName(): string {
-    return this.group.groupName
-  }
+  // public getGroupName(): string {
+  //   return this.group.groupName
+  // }
+  //
+  // public getMembersOfGroup(groupName: string): User[] {
+  //   return this.group.users
+  // }
+  //
+  // public addMember(user: User) {
+  //   if(!this.group.users.some(u => { return u.firstName === user.firstName })) {
+  //     // Ako .some vrati true, taj clan vec postoji u grupi i necemo ga dodati
+  //     this.group.users.push(user)
+  //   }
+  // }
+  //
+  // public clearGroup(): void {
+  //   this.group.users = []
+  // }
 
-  public getMembers(): User[] {
-    return this.group.users
-  }
+  public addGroup(groupName: string) {
 
-  public addMember(user: User) {
-    if(!this.group.users.some(u => { return u.firstName === user.firstName })) {
-      // Ako .some vrati true, taj clan vec postoji u grupi i necemo ga dodati
-      this.group.users.push(user)
+    if (!this.groups.some(g => {return g.groupName === groupName;})) {
+      // ako ne postoji dodajemo je u listu grupa
+      this.groups.push(new Group([], groupName));
+    } else {
+      console.log("group name must be unique")
     }
-  }
 
-  public clearGroup(): void {
-    this.group.users = []
+    console.log(this.groups);
   }
 }
